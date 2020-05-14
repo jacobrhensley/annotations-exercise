ecec6d592ed658e93f1753daf4d80bd01e48040e
Jacob Hensley <jacobrhensley3@gmail.com>
2020-05-14 15:52:37 -0500

    fixed using no annotation

--- src/Author.java
+++ src/Author.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -26,10 +26,5 @@ public class Author extends Person {
 
     public void addBook(String book) {
         books.add(book);
-    }
-
-    @Override
-    public String sortName() {
-        return String.format("%s, %s", lastName, firstName);
     }
 }
